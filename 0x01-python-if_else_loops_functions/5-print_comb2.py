ac60eb64d8e24a974a1349912bc623908638fa25
Desmends27 <desmondmends27@gmail.com>
2023-03-08 23:19:27 +0000

    more efficient

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 99):
+for i in range(0, 100):
     if i < 10:
         print("0", end='')
-    print("{:d}".format(i), end=', ')
-print("{:d}".format(i + 1))
+    print("{:d}".format(i), end=', ' if i != 99 else '\n')
