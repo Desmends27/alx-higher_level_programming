4248ff08ee173c94112ae0190802b0a25a9ba9e3
Desmends27 <desmondmends27@gmail.com>
2023-03-09 00:34:32 +0000

    fizz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if not i % 5 and not i % 3:
+        if i % 5 == 0 and not i % 3 == 0:
             print("FizzBuzz", end=' ')
         if i % 5 == 0:
             print("Buzz", end=' ')
