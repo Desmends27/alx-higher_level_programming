6b15aa9e755a0f2da5d3a8c2fa1079cb593005e3
Desmends27 <desmondmends27@gmail.com>
2023-03-11 13:08:08 +0000

    check num of argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    if len(argv) == 1:
+    if len(argv)-1 == 0:
         print("0 arguments.")
     else:
-        print("{:d} arguments:".format(len(argv)))
+        print("{:d} argument".format(len(argv) - 1), end="s:\n" if len(argv) - 1 != 1 else ":\n")
         for i in range(1, len(argv)):
             print("{0:d}: {1:s}".format(i, argv[i]))
