c82b665b4348911fafa4b45133f07ec08908e498
Desmends27 <desmondmends27@gmail.com>
2023-03-11 14:57:10 +0000

    use elif

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,23 +2,22 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
-    if len(argv[:1]) != 3:
+    if len(argv[1:]) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
     c = argv[2]
     b = int(argv[3])
     result = 0
-    match(c):
-        case '+':
+    if c == '+':
             result = add(a, b)
-        case '-':
+    elif c == '-':
             result = sub(a, b)
-        case '*':
+    elif c == '*':
             result = mul(a, b)
-        case '/':
+    elif c == '/':
             result = div(a, b)
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     print("{0:d} {1:s} {2:d} = {3:d}".format(a, c, b, result))
