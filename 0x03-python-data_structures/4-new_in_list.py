c9085fff09199094cf7cc579d363f569938050c0
Desmends27 <desmondmends27@gmail.com>
2023-03-12 17:03:41 +0000

    return none

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 and idx > len(my_list):
         return new_list
+    if list == None:
+        return None
     new_list = my_list[:]
     for i in range(len(my_list) - 1):
         if idx == i:
