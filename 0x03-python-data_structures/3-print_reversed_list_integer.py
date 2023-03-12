85af16696c7c461a623ce0dd6c2179d676b00884
Desmends27 <desmondmends27@gmail.com>
2023-03-12 17:20:32 +0000

    use array index

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@ def print_reversed_list_integer(my_list=[]):
         return None
     len_list = len(my_list)
     for i in reversed(my_list):
-        print("{:d}".format(i))
+        print("{:d}".format(my_list[i]))
