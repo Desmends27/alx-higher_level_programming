02db6952bd597860da26357416aabf236d8697c3
Desmends27 <desmondmends27@gmail.com>
2023-03-13 21:24:40 +0000

    tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
+        tup = (len(sentence), None)
+        return tup
     tup = (len(sentence), sentence[0])
     return tup
