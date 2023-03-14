9e4c2934ca61fa5d06d3f70ce46231855dac434e
Desmends27 <desmondmends27@gmail.com>
2023-03-14 20:19:07 +0000

    none

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if my_list is None:
+        return None
     if idx < 0 or idx > len(my_list):
         return my_list
-    if len(my_list) == 0:
-        return None
     for i in range(len(my_list)):
         if i == idx:
             my_list[i] = element
