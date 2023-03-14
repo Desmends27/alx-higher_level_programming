44ee47c0cfc211e9583a0678d9ca7b711c8b2aa1
Desmends27 <desmondmends27@gmail.com>
2023-03-14 19:46:17 +0000

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,37 @@
 #include "lists.h"
 /**
- * is_palindrome : checks if a linked list is a palindrome
+ * is_palindrome- checks if a linked list is a palindrome
  * @head: pointer to the the head node pointer
- * Return: 0 if not a palindrome 
+ * Return: 0 if not a palindrome
  * 1 if is palindrome
  */
 int is_palindrome(listint_t **head)
 {
 	listint_t *slow, *fast;
-	int *check = malloc(10 * sizeof(int));
+	int list_num[1000];
 	int i = 0;
 
-	if (check == NULL)
-		return NULL;
+	if (*head == NULL || (*head)->next == NULL)
+		return (0);
 	slow = *head;
 	fast = *head;
 	while (fast != NULL && fast->next != NULL)
 	{
-		fast = fast->next;
-		if (i > 10)
-		{
-			check = realloc(check, 10 * sizeof(int));
-			if (check == NULL)
-				return NULL;
-		}
-		check[i] = slow->n;
+		list_num[i++] = slow->n;
 		slow = slow->next;
-		i++;
+		fast = fast->next->next;
 	}
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
+	while (slow != NULL)
+	{
+		if ((slow->n) != list_num[--i])
+		{
+			return (0);
+		}
+		slow = slow->next;
+	}
+	return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -9,14 +11,14 @@
  * Description: singly linked list node structure
  * for project
  */
-typdef struct listint_s
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} lsitint_t;
+} listint_t;
 
-size_t prnt_listint(const listint_t *h);
-lsitint_t *add_nodeint_end(listint_t **head, const int n);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
