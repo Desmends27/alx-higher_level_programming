49b1241ef2d1df238eb9be43d8968f881cfeed19
Desmends27 <desmondopokumends2004@gmail.com>
2023-11-28 10:00:21 +0000

    modified list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,17 +10,16 @@ int check_cycle(listint_t *list)
 	const listint_t *fast;
 
 	fast = list->next;
-	slow = list->next;
+	slow = list;
 	if (list == NULL)
 		return 0;
 
 	while (fast->next != NULL && fast->next->next != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
 		if (fast == slow)
 			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
