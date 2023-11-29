b9a66aca4ecb6392cd49c33c1bfa44965bb85516
Desmends27 <desmondopokumends2004@gmail.com>
2023-11-29 15:25:30 +0000

    space after keyword

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 9):
     for j in range(1, 10):
         if j - i <= 0:
             continue
-        print("{:d}{:d}".format(i, j), end=", " if i != 8 else'')
+        print("{:d}{:d}".format(i, j), end=", " if i != 8 else '')
 print()
