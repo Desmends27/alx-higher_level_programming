c0cd7708891ce6fedb5731bc6e9c8bd4eda9ae5d
Desmends27 <desmondopokumends2004@gmail.com>
2024-01-06 22:59:50 +0000

    swapped error messages

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,9 +23,9 @@ class Rectangle:
     @height.setter
     def height(self, height):
         if type(height) is not int:
-            raise TypeError('width must be an integer')
+            raise TypeError('height must be an integer')
         elif height < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError('height must be >= 0')
         else:
             self.__height = height
 
