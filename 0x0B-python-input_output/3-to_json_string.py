a1c620147a4a21a5ed80c0736ef15f418e272bc8
Desmends27 <desmondopokumends2004@gmail.com>
2024-01-11 00:15:26 +0000

    done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import json
+
+def to_json_string(my_obj):
+    """Convert a string to json"""
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    temp = load_from_json_file("add_item.json")
+except Exception:
+    temp = []
+
+temp.extend(argv[1:])
+save_to_json_file(temp,"add_item.json")
