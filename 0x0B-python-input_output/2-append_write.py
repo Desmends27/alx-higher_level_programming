e87770658747a6d5ba466b20e3f97790688cea30
Desmends27 <desmondopokumends2004@gmail.com>
2024-01-17 18:55:28 +0000

    pep8 error

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Append a string at the end of a text file """
 
+
 def append_write(filename="", text=""):
     """ Append text to file """
     with open(filename, 'a+', encoding='utf-8') as f:
