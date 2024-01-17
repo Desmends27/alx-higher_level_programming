dda416b63917545a278c7660438013388c363800
Desmends27 <desmondopokumends2004@gmail.com>
2024-01-17 17:55:30 +0000

    add module doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" A read file function """
+
+
 def read_file(filename=""):
     """Reads a text file and prints it to stdout"""
-    with open(filename,'r', encoding = 'utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         text = f.read()
     print(text)
