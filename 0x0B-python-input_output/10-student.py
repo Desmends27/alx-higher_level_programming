ddb772f35200b57aebdcc586b066d0e2d30c7116
Desmends27 <desmondopokumends2004@gmail.com>
2024-01-27 11:30:56 +0000

    students

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Based on the student class in 9 """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+            first_name: name of the student
+            last_name: last name of student
+            age: age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation
+        all if attrs is none, others based on the string in attrs
+        """
+        if attrs:
+            attrs_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    attrs_dict[attr] = getattr(self, attr)
+            return attrs_dict
+        else:
+            return self.__dict__
