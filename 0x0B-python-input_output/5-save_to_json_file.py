0ae3681db781c434565063bc27abb2a0cbc2acd8
Desmends27 <desmondopokumends2004@gmail.com>
2024-01-27 10:57:54 +0000

    object to a text file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Save a an obj to a file """
 import json
 
+
 def save_to_json_file(my_obj, filename):
+    """ No exceptions are managed """
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(fp=f,obj=my_obj)
