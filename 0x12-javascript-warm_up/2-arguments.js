0a24a95878f49619ffd3f5ca79a85fd73d87f37e
Desmends27 <desmondopokumends2004@gmail.com>
2024-04-04 02:46:35 +0000

    new

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-if (argv.length - 2 <= 0) {
-  console.log('No arguemnt');
-} else if (argv.length - 2 === 1) {
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
